6f66fa6a3c60a80ea2c2f66e4e322d56b8f37f26
caroline <161317565+lpmtm@users.noreply.github.com>
2024-09-07 22:49:16 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+function pesquisar() { 
+    // Obtém o elemento com o ID "resultados-pesquisa" para exibir os resultados da busca
+    let section = document.getElementById("resultados-pesquisa");
+    
+    // Obtém o valor digitado no campo de pesquisa com o ID "campo-pesquisa"
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    
+    // Exibe o valor digitado no console para fins de depuração
+    console.log(campoPesquisa);
+    
+    // Se o campo de pesquisa estiver vazio, exibe uma mensagem informando que nenhuma informação foi encontrada
+    if (campoPesquisa == "") {
+        section.innerHTML = "nenhuma informacao encontrada!";
+        return; // Encerra a função para não continuar a busca
+    }
+  
+    // Converte o texto digitado no campo de pesquisa para letras minúsculas, facilitando a busca (ignora maiúsculas/minúsculas)
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    // Variáveis que serão usadas para acumular os resultados e realizar a comparação
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Percorre a lista de objetos chamada "dados" (presumivelmente vinda de um arquivo externo)
+    for (let dado of dados) {
+        // Converte o título e a descrição dos dados para letras minúsculas para fazer a comparação
+        titulo = dado.titulo.toLocaleLowerCase();
+        descricao = dado.descricao.toLocaleLowerCase();
+        tags = dado.tags.toLocaleLowerCase();
+
+        // Se o título ou a descrição conterem o texto digitado, o livro será incluído nos resultados
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)|| tags.includes(campoPesquisa)) {
+            // Monta o HTML dinâmico com os dados encontrados e acumula na variável "resultados"
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                     <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">mais informacoes</a>
+            </div>
+            `;
+        }
+    }
+
+    if(!resultados){
+        resultados = "nenhuma informacao encontrada! livro digitado nao consta no banco de dados :( tente buscar por algo diferente. "
+    }
+    
+    // Insere os resultados encontrados no elemento HTML da página (ou nada se nenhum resultado for encontrado)
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,56 @@
+let dados = [
+    {titulo: "a garota do lago", 
+    generos: "Suspense; Mistério; Ficção policial; Suspense psicológico; Policial processual.",
+    descricao: "Em Summit Lake, uma cidade tranquila à beira de um lago, a estudante de direito Becca Eckersley é brutalmente assassinada. A repórter Kelsey Castle, atraída pela tragédia, vai à cidade investigar o caso. À medida que desenterra segredos sobre a vida de Becca e suas amizades, Kelsey percebe que descobrir a verdade pode ser crucial para enfrentar os fantasmas de seu próprio passado." ,
+    autor: "Charlie Donlea",
+    publicacao: "07 de março de 2023",
+    editora: "Faro editorial", 
+    link : "https://books.google.com.br/books/about/A_garota_do_lago.html?id=KWmyEAAAQBAJ&source=kp_book_description&redir_esc=y",
+    tags: "lago misterio suspense garota assasinato"
+}
+
+
+,{titulo: "o homem de giz", 
+    generos: "Suspense; Romance; Mistério; Suspense psicológico; Romance psicológico.",
+    descricao: "Em 1986, Eddie e seus amigos usavam desenhos a giz como um código secreto em suas aventuras, até que um misterioso desenho os levou a um corpo desmembrado no bosque, mudando suas vidas para sempre. Trinta anos depois, Eddie recebe uma carta com o desenho de um homem de giz enforcado, revivendo o trauma do passado. Quando um dos amigos é encontrado morto, ele percebe que precisa desvendar o que realmente aconteceu décadas atrás para sobreviver. O Homem de Giz alterna entre passado e presente, oferecendo um suspense cheio de mistérios e reviravoltas.Em Summit Lake, uma cidade tranquila à beira de um lago, a estudante de direito Becca Eckersley é brutalmente assassinada. A repórter Kelsey Castle, atraída pela tragédia, vai à cidade investigar o caso. À medida que desenterra segredos sobre a vida de Becca e suas amizades, Kelsey percebe que descobrir a verdade pode ser crucial para enfrentar os fantasmas de seu próprio passado." ,
+    autor: "C. J. Tudor",
+    publicacao: "05 de março de 2018",
+    editora: "Intrísnseca", 
+    link : "https://www.google.com.br/books/edition/O_homem_de_giz/pGJNDwAAQBAJ?hl=pt-BR&gbpv=0",
+    tags: "giz desenhos segredo enforcado tragedia"
+}
+
+
+,{titulo: "o que aconteceu com annie", 
+    generos: "Suspense; Mistério; Terror; Suspense psicológico; Romance psicológico.",
+    descricao: "Quando adolescente, Joe Thorne viu sua irmã desaparecer, apenas para retornar de forma misteriosa. Agora, vinte e cinco anos depois, ele recebe um e-mail anônimo dizendo: -Eu sei o que aconteceu com sua irmã. Está acontecendo de novo. Endividado e fugindo de pessoas perigosas, Joe decide voltar ao vilarejo de Arnhill em busca de respostas. Porém, retornar significa enfrentar antigos segredos e abrir feridas profundas. Em um suspense com tons sobrenaturais, O Que Aconteceu com Annie conduz o leitor por reviravoltas sombrias e um passado perturbador.",
+    autor: "C. J. Tudor",
+    publicacao: "15 de maio de 2019",
+    editora: "Intrínseca", 
+    link : "https://www.google.com.br/books/edition/O_que_aconteceu_com_Annie/0dyRDwAAQBAJ?hl=pt-BR&gbpv=0",
+    tags: "annie irmã adoslecente perigosas sobrenatural "
+}
+
+,{titulo: "o lar da srta. peregrine para crianças peculiares", 
+    generos: "Romance; Ficção juvenil; Fantasia; Literatura infantil; Literatura fantástica; Terror; Fantasia sombria.",
+    descricao: "Jacob Portman cresceu ouvindo as histórias fantásticas de seu avô Abe sobre uma ilha onde moravam crianças com habilidades extraordinárias. Com o tempo, ele deixou de acreditar nessas histórias, até que, aos dezesseis anos, após a morte misteriosa de Abe, decide visitar a ilha em busca de respostas. Lá, descobre um casarão em ruínas e, ao passar por um túnel, é transportado para 1940, onde encontra as crianças peculiares que seu avô descrevia. O fascínio logo dá lugar a uma perigosa luta pela sobrevivência. O Lar da Srta. Peregrine para Crianças Peculiares mistura fantasia e suspense em um mundo cheio de mistérios." ,
+    autor: "Ransom Riggs",
+    publicacao: "12 de novembro de 2016",
+    editora: "Intrínseca", 
+    link : "https://www.google.com.br/books/edition/O_lar_da_srta_Peregrine_para_crian%C3%A7as_p/doB2DQAAQBAJ?hl=pt-BR&gbpv=0",
+    tags: "ilha crianças peculiares lar senhorita"
+}
+
+,{titulo: "boneca de ossos", 
+    generos: " Romance; Ficção juvenil; Fantasia; Literatura fantástica; História de fantasma; Ficção gótica; Ficção de aventura.",
+    descricao: "Poppy, Zach e Alice sempre foram amigos e criaram um mundo de fantasia com piratas, guerreiros e uma boneca feita de ossos chamada Grande Rainha, que governava suas brincadeiras. Mas, conforme crescem, Zach é pressionado a abandonar o faz de conta. Tudo muda quando Poppy começa a ter sonhos com a Rainha e o fantasma de uma menina que precisa ser enterrada. Os três embarcam em uma última aventura para ajudar o espírito a descansar em paz, mas logo descobrem que a jornada é mais assustadora do que imaginavam. Boneca de Ossos mistura amizade e suspense em uma história de arrepiar." ,
+    autor: "Holly Black",
+    publicacao: "10 de maio de 2014",
+    editora: "Editora Novo Conceito", 
+    link : "https://www.google.com.br/books/edition/Boneca_de_ossos/62yCAwAAQBAJ?hl=pt-BR&gbpv=0",
+    tags: "boneca piratas rainha assustador amizade"
+}
+
+];
+
+
